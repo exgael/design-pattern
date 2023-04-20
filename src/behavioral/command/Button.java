5c89c71186395919910cd6006a65bf73c03ac58b
benoit barbier <benoitbarbier.dev@gmail.com>
2023-04-20 15:07:13 -0400

    command pattern set up

---
+++ src/behavioral/command/Button.java
@@ -0,0 +1,17 @@
+package behavioral.command;
+
+public class Button {
+    private String label;
+
+    public void click() {
+        // ... ?
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
---
+++ src/behavioral/command/Main.java
@@ -0,0 +1,8 @@
+package behavioral.command;
+
+public class Main {
+    public static void main(String[] args) {
+        // Button
+        // CheckBox
+    }
+}
