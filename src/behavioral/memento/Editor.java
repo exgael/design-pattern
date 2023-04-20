6d42e1d7d7c7c30afa479b82bcfeba5057b6a526
benoit barbier <benoitbarbier.dev@gmail.com>
2023-04-20 01:33:27 -0400

    memento pattern setup

---
+++ src/behavioral/memento/Editor.java
@@ -0,0 +1,13 @@
+package behavioral.memento;
+
+public class Editor {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }    
+}
